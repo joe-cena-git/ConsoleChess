6779c6028a8dd84cf9a3b34274e0ef92c5a094d2
Joseph Cenatiempo <jcenatiempo@liquidhub.com>
2014-12-15 14:10:01 -0500

    Add Knight valid move logic

--- ConsoleChess/Knight.cs
+++ ConsoleChess/Knight.cs
@@ -12,5 +12,16 @@ namespace ConsoleChess
         {
             this.appearance = '&';
         }
+
+        public override bool isLegalMove(Tuple<int, int> origin, Tuple<int, int> destination, bool capturing)
+        {
+            if(Math.Abs(destination.Item1 - origin.Item1) == 2 && Math.Abs(destination.Item2 - origin.Item2) == 1
+                || Math.Abs(destination.Item1 - origin.Item1) == 1 && Math.Abs(destination.Item2 - origin.Item2) == 2)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
